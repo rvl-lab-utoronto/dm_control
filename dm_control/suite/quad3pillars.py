c0037ab998773ca9708da9860de81ac46b08fad3
Kevin <kevin.cheng.xie@gmail.com>
2021-01-14 23:16:26 -0500

    Fixed pillar obs env

--- dm_control/suite/quad3pillars.py
+++ dm_control/suite/quad3pillars.py
@@ -38,7 +38,8 @@ enums = mjbindings.enums
 mjlib = mjbindings.mjlib
 
 
-_DEFAULT_TIME_LIMIT = 20/2
+#_DEFAULT_TIME_LIMIT = 20/2
+_DEFAULT_TIME_LIMIT = 20
 _CONTROL_TIMESTEP = .02
 
 _N_PILLARS = 5
@@ -125,6 +126,17 @@ def reachtransferpost(time_limit=_DEFAULT_TIME_LIMIT, random=None, environment_k
   xml_string = make_model(walls=True, ball=False)
   physics = Physics.from_xml_string(xml_string, common.ASSETS)
   task = ReachTransferPillars(pretransfer=False, desired_speed=_WALK_SPEED,random=random)
+  environment_kwargs = environment_kwargs or {}
+  return control.Environment(physics, task, time_limit=time_limit,
+                             control_timestep=_CONTROL_TIMESTEP,
+                             **environment_kwargs)
+
+@SUITE.add()
+def reachtransferpostsparse(time_limit=_DEFAULT_TIME_LIMIT, random=None, environment_kwargs=None):
+  """Returns the Fetch task."""
+  xml_string = make_model(walls=True, ball=False)
+  physics = Physics.from_xml_string(xml_string, common.ASSETS)
+  task = ReachTransferPillars(pretransfer=False, desired_speed=_WALK_SPEED,random=random, sparse=True)
   environment_kwargs = environment_kwargs or {}
   return control.Environment(physics, task, time_limit=time_limit,
                              control_timestep=_CONTROL_TIMESTEP,
@@ -308,6 +320,8 @@ def _upright_reward(physics, deviation_angle=0):
       margin=1 + deviation,
       value_at_margin=0)
 
+def dist(a,b):
+  return np.linalg.norm(a-b)
 
 class ReachTransferPillars(base.Task):
   """
@@ -336,73 +350,12 @@ class ReachTransferPillars(base.Task):
     self._pillar_names = ['pillar' + str(b) for b in range(n_pillars)]
     super(ReachTransferPillars, self).__init__(random=random)
 
-  def intialize_pillars_fixed(self, physics):
-    data = physics.named.data
+  def _initialize_with_pos(self, physics, targ_pos, quad_pos, pillar_pos):
+    """
+    Attempt to initialize the objects in the env to certain positions.
+    Assumes that there are no collisions in this position or will fail.
+    """
     model = physics.named.model
-  
-  def intialize_pillars_random(self, physics):
-    data = physics.named.data
-    model = physics.named.model
-
-    spawn_radius = 0.75 * physics.named.model.geom_size['floor', 0]
-    # pillar_spawn_radius = 0.85 * physics.named.model.geom_size['floor', 0]
-    # Avoid range
-    pill2quad = 1.2
-    pill2pill = 0.6
-    quad2targ = physics.named.model.geom_size['floor', 0]
-
-    def dist(a,b):
-      return np.linalg.norm(a-b)
-
-    quad_pos = None
-    pillar_pos = None
-    penetrating = True
-    retry_count = 0
-    while penetrating:
-      retry_count += 1
-      if retry_count > 20:
-        print("Warning!!! Retrying over 20 times")
-      penetrating = False
-
-      targ_spawn_radius = 0.8 * physics.named.model.geom_size['floor', 0]
-      targ_pos = self.random.uniform(-targ_spawn_radius, targ_spawn_radius, size=(2,))
-
-      quad_pos = self.random.uniform(-spawn_radius, spawn_radius, size=(2,))
-      # print("targ",dist(targ_pos, quad_pos), quad2targ)
-      if dist(targ_pos, quad_pos) < quad2targ:
-        penetrating=True
-        continue
-
-      pillar_pos = []
-      for i in range(self._n_pillars):
-        middle_pos = (0.5*quad_pos+0.5*targ_pos) 
-        if i == 0:
-          # Put a pillar in the middle
-          pillar_pos.append(middle_pos)
-        else:
-          # average random and the middle
-          # pillar_pos.append(0.4*(0.5*quad_pos+0.5*targ_pos) + 0.6*self.random.uniform(-spawn_radius, spawn_radius, size=(2,)))
-
-          pillar_pos.append(self.random.uniform(-spawn_radius, spawn_radius, size=(2,)))
-
-          # add noise on top of middle
-          # random_pos = middle_pos + 0.5*self.random.uniform(-spawn_radius, spawn_radius, size=(2,))
-          # pillar_pos.append(np.clip(random_pos, -spawn_radius, spawn_radius))
-
-        # else:
-        #   pillar_pos.append(self.random.uniform(-spawn_radius, spawn_radius, size=(2,)))
-
-        if dist(pillar_pos[i], quad_pos) < pill2quad:
-          penetrating=True
-          continue
-        # print(dist(pillar_pos[i], quad_pos))
-        # combinatorial
-        for j in range(i):
-          if dist(pillar_pos[i], pillar_pos[j]) < pill2pill:
-            penetrating = True
-            continue
-          # print(i,j,dist(pillar_pos[i], pillar_pos[j]))
-      # print(penetrating)
 
     physics.named.model.site_pos['target', 'x'] = targ_pos[0]
     physics.named.model.site_pos['target', 'y'] = targ_pos[1]
@@ -436,6 +389,97 @@ class ReachTransferPillars(base.Task):
       if num_attempts > 10000:
         raise RuntimeError('Failed to find a non-contacting configuration.')
 
+  def intialize_pillars_fixed(self, physics):
+    data = physics.named.data
+
+    arena_radius = physics.named.model.geom_size['floor', 0]
+
+    spawn_radius = 0.75 * arena_radius
+    targ_spawn_radius = 0.8 * arena_radius
+
+    targ_pos = self.random.uniform(-targ_spawn_radius, targ_spawn_radius, size=(2,))
+    #HACK!!!
+    targ_pos = np.zeros((2,))
+    targ_pos[1] = targ_spawn_radius
+
+    quad_pos = self.random.uniform(-spawn_radius, spawn_radius, size=(2,))
+    #HACK!!!
+    quad_pos = np.zeros((2,))
+    quad_pos[1] = -spawn_radius
+
+    # Generate pillar pattern
+    offset = 0.75
+    pillar_pos = [np.array([offset*x,-0.2*abs(x)],dtype=np.float) for x in range(-2,2+1) ]
+
+    mid_pos = 0.5*(targ_pos + quad_pos)
+    # Clip if out of arena bounds
+    mid_pos_bound = arena_radius-(2*offset+0.25)
+    if mid_pos[0] > mid_pos_bound:
+      mid_pos[0] = mid_pos_bound
+    elif mid_pos[0] < -mid_pos_bound:
+      mid_pos[0] = -mid_pos_bound
+
+    pillar_pos = [x + mid_pos for x in pillar_pos] 
+    # print('pill', np.stack(pillar_pos))
+    # print('targ', targ_pos)
+
+    self._initialize_with_pos(physics, targ_pos, quad_pos, pillar_pos)
+  
+  def intialize_pillars_random(self, physics):
+    data = physics.named.data
+
+    spawn_radius = 0.75 * physics.named.model.geom_size['floor', 0]
+    # Avoid range
+    pill2quad = 1.2
+    pill2pill = 0.6
+    quad2targ = physics.named.model.geom_size['floor', 0]
+
+    quad_pos = None
+    pillar_pos = None
+    penetrating = True
+    retry_count = 0
+    while penetrating:
+      retry_count += 1
+      if retry_count > 20:
+        print("Warning!!! Retrying over 20 times")
+      penetrating = False
+
+      # Sample target and quadruped
+      targ_spawn_radius = 0.8 * physics.named.model.geom_size['floor', 0]
+      targ_pos = self.random.uniform(-targ_spawn_radius, targ_spawn_radius, size=(2,))
+      quad_pos = self.random.uniform(-spawn_radius, spawn_radius, size=(2,))
+
+      if dist(targ_pos, quad_pos) < quad2targ:
+        penetrating=True
+        continue
+
+      pillar_pos = []
+      for i in range(self._n_pillars):
+        middle_pos = (0.5*quad_pos+0.5*targ_pos) 
+        if i == 0:
+          # Put a pillar in the middle
+          pillar_pos.append(middle_pos)
+        else:
+          # average random and the middle
+          # pillar_pos.append(0.4*(0.5*quad_pos+0.5*targ_pos) + 0.6*self.random.uniform(-spawn_radius, spawn_radius, size=(2,)))
+
+          # Spawn other pillars randomly
+          pillar_pos.append(self.random.uniform(-spawn_radius, spawn_radius, size=(2,)))
+
+          # add noise on top of middle
+          # random_pos = middle_pos + 0.5*self.random.uniform(-spawn_radius, spawn_radius, size=(2,))
+          # pillar_pos.append(np.clip(random_pos, -spawn_radius, spawn_radius))
+
+        if dist(pillar_pos[i], quad_pos) < pill2quad:
+          penetrating=True
+          continue
+        # combinatorial
+        for j in range(i):
+          if dist(pillar_pos[i], pillar_pos[j]) < pill2pill:
+            penetrating = True
+            continue
+    self._initialize_with_pos(physics, targ_pos, quad_pos, pillar_pos)
+
   def initialize_episode(self, physics):
     """Sets the state of the environment at the start of each episode.
 
@@ -444,9 +488,9 @@ class ReachTransferPillars(base.Task):
 
     """
     if self._pretransfer:
-      self.intialize_pillars_random()
+      self.intialize_pillars_random(physics)
     else:
-      self.intialize_pillars_random()
+      self.intialize_pillars_fixed(physics)
 
     super(ReachTransferPillars, self).initialize_episode(physics)
 
@@ -470,14 +514,14 @@ class ReachTransferPillars(base.Task):
     if self._sparse:
       reach_reward = rewards.tolerance(
           physics.self_to_target_distance(),
-          bounds=(0, target_radius))
+          bounds=(0, 2*target_radius), sigmoid='linear',margin=4*target_radius, value_at_margin=0)
     else:
       reach_reward = rewards.tolerance(
           physics.self_to_target_distance(),
           bounds=(0, target_radius),
           sigmoid='linear',
           margin=arena_radius, value_at_margin=0)
-    return _upright_reward(physics) * reach_reward
+    return _upright_reward(physics) * (0.1+0.9*reach_reward)
 
   def _move_reward(self, physics):
     move_reward = rewards.tolerance(
